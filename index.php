f84fc8434cac6f6db4703c6b985f0464441a1624
BethoGomes <heghberthogc@gmail.com>
2023-09-30 09:08:50 -0300

    Subindo projeto

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <h1>Ola, bem vindo ao teste</h1>  
+  
+  <script src="https://clever-chat.ai/chatbot/1.0.0/index.js"></script>
+  <clever-chatbot version=1.0.0 chatbotId="3274a3f0f96"></clever-chatbot>
+
+</body>
+</html>
+
